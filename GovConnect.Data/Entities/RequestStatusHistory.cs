e0142f432d9578a0b1043ba88c73bf211dd85c0f
tanstevn <tanstevenlester@gmail.com>
2025-09-29 05:03:33 +0800

    Removed ISoftDelete inheritance to RequestStatusHistory entity

--- GovConnect.Data/Entities/RequestStatusHistory.cs
+++ GovConnect.Data/Entities/RequestStatusHistory.cs
@@ -1,14 +1,13 @@
 ﻿using GovConnect.Data.Abstractions;
 
 namespace GovConnect.Data.Entities {
-    public class RequestStatusHistory : IId, ISoftDelete {
+    public class RequestStatusHistory : IId {
         public long Id { get; set; }
         public long RequestId { get; set; }
         public short StatusId { get; set; }
         public long UpdatedBy { get; set; }
         public string? Note { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }
 
         public virtual Request? Request { get; set; }
         public virtual Status? Status { get; set; }
