1bac92dea97b7bd9a1c15dfdfeca5f5e8aee8d7d
Steven Tan <tanstevenlester@gmail.com>
2025-10-06 12:00:49 -0400

    Removing variable interfaces in virtual Configure method inside BaseConfiguration since it is not needed anymore by ConfigureSoftDelete and ConfigureId extension methods

--- GovConnect.Data/Configuration/BaseConfiguration.cs
+++ GovConnect.Data/Configuration/BaseConfiguration.cs
@@ -6,13 +6,9 @@ namespace GovConnect.Data.Configuration {
     public class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
         where TEntity : class {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder) {
-            var interfaces = typeof(TEntity)
-                .GetInterfaces()
-                .ToList();
-
             builder
-                .ConfigureSoftDelete(interfaces)
-                .ConfigureId(interfaces);
+                .ConfigureSoftDelete()
+                .ConfigureId();
         }
     }
 }
